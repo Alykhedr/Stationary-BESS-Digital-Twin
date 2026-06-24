6e8c056d6f4602ada843471596b215a5ad2a8158
Diaa Ahmed <diaagytofc@gmail.com>
2026-06-24 16:39:51 +0200

    Battery Model Ready to be "packed"
    Updated the model so it can be integrated into a pack

--- Data/Workspace_set.m
+++ Data/Workspace_set.m
@@ -11,6 +11,16 @@ if exist('surrogate_map.mat','file')
     mapCeff = double(R.C_eff);
     mapTact = double(R.t_active);
 end
+% --- BMS_Status bus object (define before the model loads) ---
+e(1) = Simulink.BusElement; e(1).Name = 'mode';          e(1).DataType = 'double';
+e(2) = Simulink.BusElement; e(2).Name = 'fault_code';    e(2).DataType = 'double';
+e(3) = Simulink.BusElement; e(3).Name = 'derate_active'; e(3).DataType = 'boolean';
+e(4) = Simulink.BusElement; e(4).Name = 'limit_hit';     e(4).DataType = 'boolean';
+e(5) = Simulink.BusElement; e(5).Name = 't_active';      e(5).DataType = 'double';
+
+BMS_Status = Simulink.Bus;
+BMS_Status.Elements = e;
+clear e
 
 save('WS_Variables.mat')
 
